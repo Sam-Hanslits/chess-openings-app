8d6c5bd67560e1045c768de25fe80a48d757be2e
Sam <samhanslits@gmail.com>
2026-04-16 16:41:30 -0400

    Removed Old Folder

--- my-vue-app/src/plugins/vuetify.ts
+++
@@ -1,7 +0,0 @@
-// src/plugins/vuetify.ts
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-
-export default createVuetify({
-  // You can add configuration options here
-})
--- my-vue-app/src/views/chessboard.d.ts
+++
@@ -1,1 +0,0 @@
-declare module '@alepot55/chessboardjs';
